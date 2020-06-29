8e928a55d6c145eb821d5495d053fc76cbc34693
fernandomarins <f.augustomarins@gmail.com>
2020-06-28 23:29:39 -0300

    added custom table cell and changed UI

---
+++ Ervas/CustomCell.swift
@@ -0,0 +1,9 @@
+//
+//  CustomCell.swift
+//  Ervas
+//
+//  Created by Fernando Augusto de Marins on 28/06/20.
+//  Copyright © 2020 Fernando Augusto de Marins. All rights reserved.
+//
+
+import Foundation
